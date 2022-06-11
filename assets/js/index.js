7a266d0fba2d9a2214b25b055a932c8cc3f025de
ir1s0047 <214749466@qq.com>
2022-06-11 09:48:34 +0800

    完成了主页功能

---
+++ assets/js/index.js
@@ -0,0 +1,38 @@
+function getUserInfo() {
+  $.ajax({
+    method: 'GET',
+    url: '/my/userinfo',
+    success: (res) => {
+      if (res.status !== 0) return layer.msg(res.message);
+      layer.msg('获取用户信息成功');
+      renderAvatar(res.data);
+    },
+  });
+}
+// 渲染用户头像
+const renderAvatar = (user) => {
+  // 获取用户名字
+  let name = user.nickname || user.username;
+  // 设置欢迎文本
+  $('#welcome').html(`欢迎 ${name}`);
+  // 按需渲染用户头像
+  if (user.user_pic !== null) {
+    // 渲染图片头像
+    $('.layui-nav-img').attr('src', user.user_pic).show();
+    $('.text-avatar').hide();
+  } else {
+    // 渲染文本头像
+    $('.layui-nav-img').hide();
+    let firstName = name[0].toUpperCase();
+    $('.text-avatar').html(firstName);
+  }
+};
+// 退出登录
+$('#btnLogout').click(() => {
+  layer.confirm('是否退出登录?', { icon: 3, title: '提示' }, function (index) {
+    localStorage.removeItem('token');
+    location.href = '/login.html';
+  });
+});
+//获取用户列表
+getUserInfo();
